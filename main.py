0fcca677e8cc92dffc41adebcb30d3b37dd6e487
21harley <john.llanes.1996@gmail.com>
2023-01-30 23:23:09 -0400

    add . route

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ app=Flask(__name__)
 @app.route("/")
 def hello_wordl():
     return render_template("index.html")
+
+@app.route("/contacto",defaults={"correo":'-'})
+@app.route("/contacto/<correo>")
+def fn_contacto(correo):
+    return render_template('contacto.html',usuario=correo)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
